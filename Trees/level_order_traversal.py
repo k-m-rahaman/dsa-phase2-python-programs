2290da1ad5993de8fbc714b9e545734c183dc631
k-m-rahaman <kmrofficial786@gmail.com>
2026-04-10 05:28:02 +0530

    Added level order traversal (BFS) in Python

---
+++ Trees/level_order_traversal.py
@@ -0,0 +1,16 @@
+from collections import deque
+
+def level_order(root):
+    if not root:
+        return
+
+    queue = deque([root])
+
+    while queue:
+        node = queue.popleft()
+        print(node.data, end=" ")
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
