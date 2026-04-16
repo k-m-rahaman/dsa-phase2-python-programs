17a4d87470ac5ead26bf4b424ef078c4188b9e62
k-m-rahaman <kmrofficial786@gmail.com>
2026-04-17 05:01:20 +0530

    Added Kadane algorithm in Python

---
+++ Two Pointer/max_subarray_sum.py
@@ -0,0 +1,9 @@
+arr = [-2,1,-3,4,-1,2,1,-5,4]
+
+max_sum = current = arr[0]
+
+for num in arr[1:]:
+    current = max(num, current + num)
+    max_sum = max(max_sum, current)
+
+print("Max Sum:", max_sum)
